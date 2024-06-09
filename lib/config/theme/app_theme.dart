41a53b0a2a2f078905639fe6d21ede0f603e4f1e
Isj-Code <isj.code@gmail.com>
2024-06-09 22:07:45 +0200

    SimpleNotifier

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,26 +1,17 @@
-
-
 import 'package:flutter/material.dart';
 
-
-const seedColor = Color.fromARGB(255, 7, 80, 59);
+const seedColor = Color.fromARGB(255, 13, 110, 167);
 
 class AppTheme {
-
   final bool isDarkmode;
 
-  AppTheme({ required this.isDarkmode });
-
+  AppTheme({required this.isDarkmode});
 
   ThemeData getTheme() => ThemeData(
-    useMaterial3: true,
-    colorSchemeSeed: seedColor,
-    brightness: isDarkmode ? Brightness.dark : Brightness.light,
-
-    listTileTheme: const ListTileThemeData(
-      iconColor: seedColor,
-    ),
-    
-  );
-
+        colorSchemeSeed: seedColor,
+        brightness: isDarkmode ? Brightness.dark : Brightness.light,
+        listTileTheme: const ListTileThemeData(
+          iconColor: seedColor,
+        ),
+      );
 }
